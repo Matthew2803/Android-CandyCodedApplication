3858128f8b10d4557f667c5236e0beddda93de24
ndivhuwo <cliff.gf87@gmail.com>
2020-09-26 21:03:47 +0200

    commit&push

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,11 +41,13 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     public void createPhoneIntent(View view){
-        Uri tIntentUri = Uri.parse("tel:0123456789");
-        Intent phoneIntent = new Intent(Intent.ACTION_DIAL,tIntentUri);
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
         startActivity(phoneIntent);
+    }
 
-
+    private Object encode(String s) {
+        return null;
     }
     // ***
 }
